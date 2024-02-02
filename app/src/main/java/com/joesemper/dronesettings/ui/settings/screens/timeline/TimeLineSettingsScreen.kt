ab98d1e3b862eb07f4051adc23cc2501fb278f9e
JoeSemper <joe.semper77@gmail.com>
2024-02-02 16:47:30 +0300

    New timeline screen layout

--- app/src/main/java/com/joesemper/dronesettings/ui/settings/screens/timeline/TimeLineSettingsScreen.kt
+++ app/src/main/java/com/joesemper/dronesettings/ui/settings/screens/timeline/TimeLineSettingsScreen.kt
@@ -6,17 +6,11 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,19 +19,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.joesemper.dronesettings.R
-import com.joesemper.dronesettings.ui.settings.CheckboxWithText
 import com.joesemper.dronesettings.ui.settings.ParameterCardView
 import com.joesemper.dronesettings.ui.settings.SettingsDefaultScreenContainer
-import com.joesemper.dronesettings.ui.settings.TimeLayout
 import com.joesemper.dronesettings.ui.settings.TimeSelectDialog
-import com.joesemper.dronesettings.ui.settings.TitleWithSubtitleView
 import com.joesemper.dronesettings.ui.settings.state.SettingsUiAction
 import com.joesemper.dronesettings.ui.settings.state.delayTimeValidator
 import com.joesemper.dronesettings.ui.settings.state.rememberTimeSelectDialogState
@@ -98,63 +87,19 @@ fun TimelineScreenContent(
     onUiEvent: (TimelineUiEvent) -> Unit
 ) {
     Column(
-        modifier = modifier
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-
-        var enabled by remember { mutableStateOf(true) }
-
-        ParameterCardView(
-            modifier = Modifier.padding(vertical = 8.dp),
-            title = stringResource(id = R.string.delay_time),
-            onClick = {},
-            content = {
-                Text(
-                    text = "10 : 30",
-                    style = MaterialTheme.typography.displayMedium
-                )
-            }
-        )
-
-        ParameterCardView(
-            modifier = Modifier.padding(vertical = 8.dp),
-            title = stringResource(id = R.string.delay_time),
-            switchable = true,
-            enabled = enabled,
-            onEnabledChange = { enabled = it },
-            onClick = {},
-            content = {
-                Text(
-                    text = "10 : 30",
-                    style = MaterialTheme.typography.displayMedium
-                )
-            }
-        )
-
-        ParameterCardView(
-            modifier = Modifier.padding(vertical = 8.dp),
-            title = stringResource(id = R.string.delay_time),
-            onClick = {},
-            content = {
-                Text(
-                    text = "10 : 30",
-                    style = MaterialTheme.typography.displayMedium
-                )
-            }
-        )
 
         DelayTimeSettingsView(
             state = state.delayTimeState,
             onUiEvent = onUiEvent
         )
 
-        Divider(modifier = Modifier.fillMaxWidth())
-
         CockingTimeSettingsView(
             state = state.cockingTimeState,
             onUiEvent = onUiEvent
         )
-
-        Divider(modifier = Modifier.fillMaxWidth())
 
         MaximumTimeSettingsView(
             state = state.selfDestructionTimeState,
@@ -193,38 +138,18 @@ fun DelayTimeSettingsView(
         )
     }
 
-    Column(
-        modifier = modifier.padding(vertical = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-
-        TitleWithSubtitleView(
-            title = stringResource(R.string.delay_time),
-            subtitle = stringResource(R.string.delay_until_all_systems_activation),
-        )
-
-        Row(
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Icon(
-                modifier = Modifier.size(32.dp),
-                painter = painterResource(id = R.drawable.alarm),
-                contentDescription = null
+    ParameterCardView(
+        modifier = modifier,
+        title = stringResource(id = R.string.delay_time),
+        onClick = { showDialog = true },
+        content = {
+            Text(
+                text = "${state.time.minutes} : ${state.time.seconds}",
+                style = MaterialTheme.typography.displayMedium
             )
-
-            TimeLayout(
-                minutes = state.time.minutes,
-                seconds = state.time.seconds,
-                isError = state.error.isMinutesError,
-                onMinutesClick = { showDialog = true },
-                onSecondsClick = { showDialog = true }
-            )
-
-            Spacer(modifier = Modifier.size(48.dp))
         }
+    )
 
-    }
 }
 
 @Composable
@@ -257,46 +182,21 @@ fun CockingTimeSettingsView(
         )
     }
 
-    Column(
-        modifier = modifier.padding(vertical = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-
-        TitleWithSubtitleView(
-            title = stringResource(R.string.cocking_time),
-            subtitle = stringResource(R.string.cocking_time_description),
-        )
-
-        CheckboxWithText(
-            modifier = Modifier.fillMaxWidth(),
-            text = stringResource(R.string.activate_cocking_time),
-            checked = state.enabled,
-            onCheckedChange = { onUiEvent(TimelineUiEvent.CockingTimeActivationChange(it)) }
-        )
-
-        Row(
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Icon(
-                modifier = Modifier.size(32.dp),
-                painter = painterResource(id = R.drawable.alarm),
-                contentDescription = null,
+    ParameterCardView(
+        modifier = modifier,
+        title = stringResource(id = R.string.cocking_time),
+        switchable = true,
+        enabled = state.enabled,
+        onEnabledChange = {onUiEvent(TimelineUiEvent.CockingTimeActivationChange(it))},
+        onClick = { showDialog = true },
+        content = {
+            Text(
+                text = "${state.time.minutes} : ${state.time.seconds}",
+                style = MaterialTheme.typography.displayMedium
             )
-
-            TimeLayout(
-                minutes = state.time.minutes,
-                seconds = state.time.seconds,
-                enabled = state.enabled,
-                isError = state.error.isMinutesError,
-                onMinutesClick = { showDialog = true },
-                onSecondsClick = { showDialog = true }
-            )
-
-            Spacer(modifier = Modifier.size(48.dp))
         }
+    )
 
-    }
 }
 
 @Composable
@@ -329,36 +229,16 @@ fun MaximumTimeSettingsView(
         )
     }
 
-    Column(
-        modifier = modifier.padding(vertical = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-
-        TitleWithSubtitleView(
-            title = stringResource(R.string.maximum_time),
-            subtitle = stringResource(R.string.self_destruction_time),
-        )
-
-        Row(
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Icon(
-                modifier = Modifier.size(32.dp),
-                painter = painterResource(id = R.drawable.alarm),
-                contentDescription = null
+    ParameterCardView(
+        modifier = modifier,
+        title = stringResource(id = R.string.maximum_time),
+        onClick = { showDialog = true },
+        content = {
+            Text(
+                text = "${state.time.minutes} : ${state.time.seconds}",
+                style = MaterialTheme.typography.displayMedium
             )
-
-            TimeLayout(
-                minutes = state.time.minutes,
-                seconds = state.time.seconds,
-                isError = state.error.isMinutesError,
-                onMinutesClick = { showDialog = true },
-                onSecondsClick = { showDialog = true }
-            )
-
-            Spacer(modifier = Modifier.size(48.dp))
         }
+    )
 
-    }
 }
